c06fd0af7957cc7404255a560710727e39f82f41
Stephen Cherry <35240736+Narolith@users.noreply.github.com>
2023-04-02 20:56:44 -0500

    Added code to add src to sys.path

--- src/cogs/buff_reminder_service.py
+++ src/cogs/buff_reminder_service.py
@@ -17,7 +17,8 @@ import pytz
 
 from discord.ext import commands, tasks
 
-from src.datetime_helper import calculate_buff_due_time
+
+from datetime_helper import calculate_buff_due_time
 
 
 class BuffReminderService(commands.Cog):
