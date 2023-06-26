d445d87e1cfb07fccf7be0786ec9d0793a672c2a
Stephen Cherry <stephen.m.cherry91@gmail.com>
2023-06-26 01:18:13 -0500

    separated response from other checks

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -4,15 +4,16 @@ import { BuffService } from "../services/BuffService";
 export const onMessage = async (message: Message) => {
   const currentDate = new Date();
   const currentHour: number = currentDate.getUTCHours();
-  if (
-    message.channelId === process.env.BUFF_CHANNEL_ID &&
-    BuffService.validHours.includes(currentHour) &&
-    BuffService.CheckCooldown(new Date().getTime()) &&
-    message.content.includes(":BuffCat:")
-  ) {
+  if (message.content.includes(":BuffCat:")) {
     message.reply("Praise be to the Buff Cat!");
-    currentDate.setUTCHours(currentHour + 6);
-    BuffService.SetBuffsUpdated(true);
-    BuffService.SetCooldown(currentDate.getTime());
+    if (
+      message.channelId === process.env.BUFF_CHANNEL_ID &&
+      BuffService.validHours.includes(currentHour) &&
+      BuffService.CheckCooldown(new Date().getTime())
+    ) {
+      currentDate.setUTCHours(currentHour + 6);
+      BuffService.SetBuffsUpdated(true);
+      BuffService.SetCooldown(currentDate.getTime());
+    }
   }
 };
