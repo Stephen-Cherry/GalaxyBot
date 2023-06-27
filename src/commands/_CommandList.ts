d19dc51c6b694101ec065e5b41527d696a82ffb8
Stephen Cherry <stephen.m.cherry91@gmail.com>
2023-06-26 21:16:36 -0500

    Added github command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,5 +1,6 @@
 import { Command } from "../interfaces/Command";
+import { github } from "./github";
 import { info } from "./info";
 import { server } from "./server";
 
-export const CommandList: Command[] = [info, server];
+export const CommandList: Command[] = [info, server, github];
