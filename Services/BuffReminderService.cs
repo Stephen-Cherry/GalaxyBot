c825927eaf680bcdff325f7f623ee80109b9db2e
Stephen Cherry <stephen.m.cherry91@gmail.com>
2023-07-19 21:09:03 -0500

    Cleaned up variables

--- Services/BuffReminderService.cs
+++ Services/BuffReminderService.cs
@@ -5,22 +5,19 @@ using Microsoft.Extensions.Configuration;
 namespace GalaxyBot.Services;
 public class BuffReminderService
 {
-    private static readonly List<int> validUpdateHoursUtc = new() { 0, 1, 2, 3, 4 };
     private static bool hasUpdated = false;
     private readonly DiscordSocketClient _client;
     private readonly IConfiguration _configuration;
-    private readonly CronExpression _cronExpression;
-    private readonly TimeZoneInfo _centralTimeZone;
 
     public BuffReminderService(DiscordSocketClient client, IConfiguration configuration)
     {
         _client = client;
         _configuration = configuration;
 
-        _cronExpression = CronExpression.Parse("@midnight");
-        _centralTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+        CronExpression cronExpression = CronExpression.Parse("@midnight");
+        TimeZoneInfo centralTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
 
-        TaskSchedulerService.ScheduleJob(_cronExpression, _centralTimeZone, async () =>
+        TaskSchedulerService.ScheduleJob(cronExpression, centralTimeZone, async () =>
         {
             if (hasUpdated)
             {
@@ -51,7 +48,7 @@ public class BuffReminderService
                 && !isBot)
             {
                 userMessage.Channel.SendMessageAsync("Praise be to the buff cat!");
-                bool isUpdateHour = validUpdateHoursUtc.Contains(DateTime.UtcNow.Hour);
+                bool isUpdateHour = Constants.VALID_BUFF_UPDATE_HOURS_UTC.Contains(DateTime.UtcNow.Hour);
                 if (isUpdateHour)
                 {
                     hasUpdated = true;
