b75a191197ab13b57081296ff753c0e1cc5a350f
Stephen Cherry <stephen.m.cherry91@gmail.com>
2023-07-19 20:46:56 -0500

    Created Constants file

---
+++ Constants.cs
@@ -0,0 +1,10 @@
+
+namespace GalaxyBot;
+
+public static class Constants
+{
+    public static readonly string CONNECTION_STRING = "GalaxyBotDatabase";
+    public static readonly string BUFF_CHANNEL_KEY = "BuffChannelId";
+    public static readonly string BUFF_CAT_EMOTE = ":BuffCat:";
+
+}
