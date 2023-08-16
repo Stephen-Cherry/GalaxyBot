9ac75e74b82247476fa1804443a48743c69d5add
Stephen Cherry <stephen.m.cherry91@gmail.com>
2023-08-16 16:19:28 -0500

    moved config to helper method

--- Extensions/IServiceCollectionExtensions.cs
+++ Extensions/IServiceCollectionExtensions.cs
@@ -5,19 +5,19 @@ public static class IServiceCollectionExtensions
     public static void AddApplicationServices(this IServiceCollection services)
     {
         services.AddDbContextFactory<GalaxyBotContext>();
-        services.AddSingleton(
-            new DiscordSocketConfig()
-            {
-                GatewayIntents =
-                    GatewayIntents.GuildMembers
-                    | GatewayIntents.AllUnprivileged
-                    | GatewayIntents.MessageContent
-            }
-        );
+        services.AddSingleton(GetDiscordSocketConfig());
         services.AddSingleton<DiscordSocketClient>();
         services.AddSingleton(new InteractionServiceConfig());
         services.AddSingleton<InteractionService>();
         services.AddSingleton<LoggingService>();
         services.AddSingleton<BuffReminderService>();
+    }
+
+    private static DiscordSocketConfig GetDiscordSocketConfig()
+    {
+        return new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.GuildMembers | GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
     }
 }
