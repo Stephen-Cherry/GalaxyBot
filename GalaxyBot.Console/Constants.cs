f966a0ea024decf8bbaaa2597034978dbd5b9187
Stephen Cherry <stephen.m.cherry91@gmail.com>
2023-08-17 20:56:34 -0500

    Consolidated constants

--- GalaxyBot.Console/Constants.cs
+++ GalaxyBot.Console/Constants.cs
@@ -4,11 +4,9 @@ public static class Constants
 {
     public static readonly string BUFF_CHANNEL_ID = "BUFF_CHANNEL_ID";
     public static readonly string BUFF_CAT_EMOTE = ":BuffCat:";
-    public static readonly string CRON_MIDNIGHT = "@midnight";
     public static readonly string LOG_CHANNEL_ID = "LOG_CHANNEL_ID";
-    public static readonly string TIME_ZONE = "Central Standard Time";
     public static readonly string TOKEN = "TOKEN";
-    public static readonly CronExpression CRON_DAILY_MIDNIGHT_EXPRESSION = CronExpression.Parse(CRON_MIDNIGHT);
-    public static readonly TimeZoneInfo CENTRAL_TIMEZONE_INFO = TimeZoneInfo.FindSystemTimeZoneById(TIME_ZONE);
+    public static readonly CronExpression CRON_DAILY_MIDNIGHT_EXPRESSION = CronExpression.Parse("@midnight");
+    public static readonly TimeZoneInfo CENTRAL_TIMEZONE_INFO = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
     public static readonly ReadOnlyCollection<int> BUFF_HOURS = new List<int>() { 17, 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4 }.AsReadOnly();
 }
