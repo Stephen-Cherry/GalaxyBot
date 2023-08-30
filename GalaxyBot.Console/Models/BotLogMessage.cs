ad94d8da76fe0bb688255d646a7655a430196514
Stephen Cherry <Cherry.Stephen.M@Outlook.com>
2023-08-29 20:32:12 -0500

    Added console logging.

--- GalaxyBot.Console/Models/BotLogMessage.cs
+++ GalaxyBot.Console/Models/BotLogMessage.cs
@@ -2,8 +2,13 @@
 
 public class BotLogMessage
 {
-    public int Id { get; set; }
-    public required string Message { get; set; }
-    public required LogSeverity Severity { get; set; }
-    public string? Source { get; set; }
+  public int Id { get; set; }
+  public required string Message { get; set; }
+  public required LogSeverity Severity { get; set; }
+  public string? Source { get; set; }
+
+  public override string ToString()
+  {
+    return $"{Source}/{Severity}/{Message}";
+  }
 }
