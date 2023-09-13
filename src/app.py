dd518e514a0e545a081b0093705a9e60010242a0
Stephen Cherry <cherry.stephen.m@outlook.com>
2023-09-13 10:48:56 -0500

    Modified extension import

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from typing import List
+import pkgutil
 
 from dotenv import load_dotenv
 from interactions import Client, Intents
@@ -20,11 +21,11 @@ def main():
         intents=Intents.GUILD_MESSAGES | Intents.GUILDS | Intents.MESSAGE_CONTENT,
         logger=cls_log,
     )
-
-    for file in os.listdir("src/cogs"):
-        if file.endswith(".py"):
-            bot.load_extension(f"cogs.{file[:-3]}")
-    bot.start(os.environ.get("GALAXY_TOKEN"))
+    extension_names = [
+        m.name for m in pkgutil.iter_modules(["extensions"], prefix="extensions.")
+    ]
+    for extension in extension_names:
+        bot.load_extension(extension)
 
 
 def validate_secrets() -> List[str]:
@@ -32,6 +33,7 @@ def validate_secrets() -> List[str]:
     token: str | None = os.environ.get("GALAXY_TOKEN")
     if not token:
         invalid_secrets.append("GALAXY_TOKEN")
+    return invalid_secrets
 
 
 if __name__ == "__main__":
